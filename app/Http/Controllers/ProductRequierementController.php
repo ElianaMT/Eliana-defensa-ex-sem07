255b2b667fd0b8aba42834ed2606f04a371c4c23
Eliana Elizabeth Morillo Taco <eliana_taco@estudante.sesisenai.org.br>
2023-11-21 11:28:48 -0500

    feat: lista requierements_products

--- app/Http/Controllers/ProductRequierementController.php
+++ app/Http/Controllers/ProductRequierementController.php
@@ -1,10 +1,19 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\ProductRequierement;
 use Illuminate\Http\Request;
 
 class ProductRequierementController extends Controller
 {
-    //
+    public function index( Request $request){
+
+        $product_id = $request->query('product_id');
+
+        $productsRequierement = ProductRequierement::query()
+        -> where('product_id', $product_id)
+        -> get();
+       
+        return $productsRequierement;
+     }
 }
