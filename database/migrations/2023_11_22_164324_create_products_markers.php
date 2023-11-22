4cb0ca0f90eed348a7c8772c4cef66b16597ddde
Eliana Elizabeth Morillo Taco <eliana_taco@estudante.sesisenai.org.br>
2023-11-22 11:48:44 -0500

    feat:controller

--- database/migrations/2023_11_22_164324_create_products_markers.php
+++ database/migrations/2023_11_22_164324_create_products_markers.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('products_markers', function (Blueprint $table) {
             $table->id();
+           
             $table->timestamps();
         });
     }
