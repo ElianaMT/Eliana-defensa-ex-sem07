a84c0cf4f8a6cbb203d2b7eae1e35c2692474564
Eliana Elizabeth Morillo Taco <eliana_taco@estudante.sesisenai.org.br>
2024-01-03 01:50:07 -0500

    feat: cria metodo store

--- app/Http/Controllers/AvaliationController.php
+++ app/Http/Controllers/AvaliationController.php
@@ -1,10 +1,30 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Avaliation;
+use Exception;
 use Illuminate\Http\Request;
 
 class AvaliationController extends Controller
 {
-    //
+    public function store(Request $request){
+        try {
+            $request->validate([
+                'product_id' => 'required|integer',
+                'description' => 'string',
+                'recommended' => 'boolean',
+
+            ]);
+
+            $data = $request->all();
+
+            $avaliation = Avaliation::create($data);
+
+            return $avaliation;
+
+        } catch (Exception $exception) {
+            return response()->json(['message' => $exception->getMessage()], 400);
+        }
+
+    }
 }
